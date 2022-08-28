7484c89b00ddf22ee578d29c508bbba9961987a3
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-08-28 01:09:30 +0100

    lint

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,14 +2,11 @@
 import { createRouter } from './context'
 import superjson from 'superjson'
 
-import { exampleRouter } from './example'
 import { playlistRouter } from './playlists'
-import { authRouter } from './auth'
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('playlists.', playlistRouter)
-  .merge('auth.', authRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
