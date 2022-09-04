58e2d249390ead381d6e6a66eec484e945de94fc
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-09-04 11:40:48 +0100

    add a message if no tracks in playlist

--- src/components/ui/playlists/PlaylistElement.tsx
+++ src/components/ui/playlists/PlaylistElement.tsx
@@ -72,59 +72,76 @@ export default function PlaylistElement({
       className='playlist-element-container bg-gray-800/40'
       ref={container}
     >
-      <ul>
-        {playlist.pages.map((group: any, i: number) => (
-          <React.Fragment key={i}>
-            {group.items?.map((item: any, index: number) => (
-              <li
-                className={`p-4 ${
-                  toHighlight?.id === item?.track?.id
-                    ? `highlight ${item?.track?.id}`
-                    : ''
-                } transition-all bg-gray-800/40 flex items-center gap-3 hover:bg-green-900`}
-                key={index}
-              >
-                {/* set correct index depending on page */}
-                {i + 1 >= 2 ? <p>{index + 1 + 100}</p> : <p>{index + 1}</p>}
-                <img
-                  src={item.track.album.images[0].url}
-                  className='w-10'
-                  alt={item.track.name}
-                />
-                <div className='track-name-artist'>
-                  <h4>{item.track.name}</h4>
-                  <h5 className='text-neutral-300 text-sm'>
-                    {item.track.artists.map((artist: any, index: number) => (
-                      <span key={index}>
-                        {index === 0 ? (
-                          <span>
-                            {artist.name}
-                            {item.track.artists.length > 1 && (
-                              <span className='text-neutral-400'>{' / '}</span>
-                            )}
-                          </span>
-                        ) : (
-                          <span className='text-neutral-400'>
-                            {artist.name}
-                            {item.track.artists.length > 2 &&
-                              index !== item.track.artists.length - 1 && (
-                                <span>{' / '}</span>
+      {playlist.pages[0]?.items?.length ? (
+        <ul>
+          {playlist.pages.map((group: any, i: number) => (
+            <React.Fragment key={i}>
+              {group.items?.map((item: any, index: number) => (
+                <li
+                  className={`p-4 ${
+                    toHighlight?.id === item?.track?.id
+                      ? `highlight ${item?.track?.id}`
+                      : ''
+                  } transition-all bg-gray-800/40 flex items-center gap-3 hover:bg-green-900`}
+                  key={index}
+                >
+                  {/* set correct index depending on page */}
+                  {i + 1 >= 2 ? <p>{index + 1 + 100}</p> : <p>{index + 1}</p>}
+                  <img
+                    src={item.track.album.images[0].url}
+                    className='w-10'
+                    alt={item.track.name}
+                  />
+                  <div className='track-name-artist'>
+                    <h4>{item.track.name}</h4>
+                    <h5 className='text-neutral-300 text-sm'>
+                      {item.track.artists.map((artist: any, index: number) => (
+                        <span key={index}>
+                          {index === 0 ? (
+                            <span>
+                              {artist.name}
+                              {item.track.artists.length > 1 && (
+                                <span className='text-neutral-400'>
+                                  {' / '}
+                                </span>
                               )}
-                          </span>
-                        )}
-                      </span>
-                    ))}
-                  </h5>
-                </div>
-                <RiDeleteBin5Line
-                  onClick={() => deleteTrack.mutate(item.track.uri)}
-                  className='ml-auto text-gray-400 hover:text-red-600 cursor-pointer'
-                />
-              </li>
-            ))}
-          </React.Fragment>
-        ))}
-      </ul>
+                            </span>
+                          ) : (
+                            <span className='text-neutral-400'>
+                              {artist.name}
+                              {item.track.artists.length > 2 &&
+                                index !== item.track.artists.length - 1 && (
+                                  <span>{' / '}</span>
+                                )}
+                            </span>
+                          )}
+                        </span>
+                      ))}
+                    </h5>
+                  </div>
+                  <RiDeleteBin5Line
+                    onClick={() => deleteTrack.mutate(item.track.uri)}
+                    className='ml-auto text-gray-400 hover:text-red-600 cursor-pointer'
+                  />
+                </li>
+              ))}
+            </React.Fragment>
+          ))}
+        </ul>
+      ) : (
+        <div
+          style={{ minHeight: '300px' }}
+          className='grid p-3 place-content-center text-center'
+        >
+          <h2 className='text-5xl font-extrabold tracking-tighter'>Woah!</h2>
+          <h3 className='text-xl text-gray-400'>
+            There are no tracks in this playlist!
+          </h3>
+          <h4 className='text-md text-gray-400'>
+            You can add them using the recommendations or the search feature
+          </h4>
+        </div>
+      )}
     </div>
   )
 }
