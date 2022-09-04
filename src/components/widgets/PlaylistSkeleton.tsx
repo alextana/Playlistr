ef4b8fb5c68d9037111bf404fc18544b87346b60
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-09-04 13:36:26 +0100

    add correct rounding for playlist items

--- src/components/widgets/PlaylistSkeleton.tsx
+++ src/components/widgets/PlaylistSkeleton.tsx
@@ -7,7 +7,7 @@ export default function PlaylistSkeleton() {
         className='skeleton bg-gray-800/80 mb-2'
         style={{ width: '300px', height: '40px' }}
       ></div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6'>
         {Array(20)
           .fill('')
           .map(() => (
